b8cd2e06b696a3973391748aea09c46e1769df70
rengoku <gamerelbistanli@gmail.com>
2026-03-03 03:32:43 +0300

    ödev

---
+++ oopdersprogrami/src/a.java
@@ -0,0 +1,29 @@
+
+
+public class a {
+    String mat;
+    String maincourse;
+    String writing;
+    String speaking;
+    String nativ;
+    String alm;
+    String edb;
+    String resim;
+    String btk;
+    String beden;
+    String rehberlik;
+    public a(String mat, String maincourse, String writing, String speaking, String nativ, String alm, String edb, String resim, String btk, String beden,String rehberlik){
+        this.mat=mat;
+        this.maincourse=maincourse;
+        this.writing =writing;
+        this.speaking = speaking;
+        this.nativ =nativ;
+        this.alm=alm;
+        this.edb=edb;
+        this.resim=resim;
+        this.btk=btk;
+        this.beden=beden;
+        this.rehberlik=rehberlik;
+
+    }
+}
---
+++ oopdersprogrami/src/b.java
@@ -0,0 +1,30 @@
+public class b {
+    String mathc;
+    String maincoursehc;
+    String writinghc;
+    String speakinghc;
+    String nativhc;
+    String almhc;
+    String edbhc;
+    String resimhc;
+    String btkhc;
+    String bedenhc;
+    String rehberlikhc;
+
+
+
+
+    public b(String mathc, String maincoursehc, String writinghc, String speakinghchc, String nativhc, String almhc, String edbhc, String resimhc, String btkhc, String bedenhc,String rehberlikhc){
+        this.mathc=mathc;
+        this.maincoursehc = maincoursehc;
+        this.writinghc = writinghc;
+        this.speakinghc = speakinghchc;
+        this.nativhc = nativhc;
+        this.almhc=almhc;
+        this.edbhc=edbhc;
+        this.resimhc=resimhc;
+        this.btkhc=btkhc;
+        this.bedenhc=bedenhc;
+        this.rehberlikhc=rehberlikhc;
+    }
+}
